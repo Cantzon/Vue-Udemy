8a4e50e051926cf9738b0f7226bc835701b2d0c8
Hari Krishna <harikrishna98@gmail.com>
2021-04-18 12:45:00 +0300

    removed CLI folder and created first CLI app under assignments

---
+++ Lectures/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<section>
+  <h2> Friend List</h2>
+  <ul>
+    <li>
+
+    </li>
+  </ul>
+</section>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      friends: [
+        {
+          name: 'Bruh Moment',
+          phone: '123 456',
+          email: 'bruh@moment'
+        }, {
+          name: 'Baba Booey',
+          phone: '789 012',
+          email: 'stop@dude.com'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
