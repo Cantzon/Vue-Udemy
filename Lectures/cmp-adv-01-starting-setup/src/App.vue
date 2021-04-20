118805794bf9a8f836e4136b80d17b2c11493784
Hari Krishna <harikrishna98@gmail.com>
2021-04-20 13:26:50 +0300

    added new project and also scoped the styles

---
+++ Lectures/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!--    <TheHeader />-->
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+        :full-name="activeUser.name"
+        :info-text="activeUser.description"
+        :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    // 'the-header': TheHeader
+    TheHeader,
+    BadgeList,
+    UserInfo
+
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
