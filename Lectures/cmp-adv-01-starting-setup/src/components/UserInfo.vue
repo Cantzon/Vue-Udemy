ad0f0f18d2382c140e4ff711bdb5adc65282d7d1
Hari Krishna <harikrishna98@gmail.com>
2021-04-20 13:53:45 +0300

    added slots and named them to help style html sections, slots kind of serving like components but for html

--- Lectures/cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ Lectures/cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,27 +1,29 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:header>
+        <header>
+          <h3>{{ fullName }}</h3>
+          <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+        </header>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
 <script>
+import BaseCard from "@/components/BaseCard";
+
 export default {
+  components: {BaseCard},
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
 
 <style scoped>
-section  {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
 
 section header {
   display: flex;
