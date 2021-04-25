a4df14da9852bb061a0d570067fda3e6389428d0
Hari Krishna <harikrishna98@gmail.com>
2021-04-25 11:14:12 +0300

    created error dialog for empty goal

--- Lectures/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ Lectures/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
     <h2> Manage Goals </h2>
-    <input type="text">
+    <input type="text" ref="goal" />
+    <button @click="addGoal"> Add Goal </button>
+    <error-alert v-if="showError">
+      <h2> Error!</h2>
+      <p> Please enter a valid goal. </p>
+      <button @click="showError = !showError"> Chill okay dude  </button>
+    </error-alert>
   </div>
 </template>
 
 <script>
+import ErrorAlert from "@/components/ErrorAlert";
 export default {
-  name: "ManageGoals"
+  name: "ManageGoals",
+  components: {ErrorAlert},
+  data() {
+    return {
+      showError: false,
+    }
+  },
+  methods: {
+    addGoal() {
+      const enteredVal = this.$refs.goal.value;
+      if (enteredVal === ''){
+        this.showError = true;
+      }
+    }
+  }
 }
 </script>
 
