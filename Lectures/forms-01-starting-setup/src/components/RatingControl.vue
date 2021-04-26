0e7d872e6bb5f1ca8ca4cba75d1b98074546a2e4
Hari Krishna <harikrishna98@gmail.com>
2021-04-26 16:25:03 +0300

    finished forms lecture and added http lecture

---
+++ Lectures/forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul>
+    <li :class='{active: modelValue === "poor"}'>
+      <button type='button' @click='activate("poor")'>Poor</button>
+    </li>
+    <li :class='{active: modelValue === "avg"}'>
+      <button type='button' @click='activate("avg")'>Average</button>
+    </li>
+    <li :class='{active: modelValue === "great"}'>
+      <button type='button' @click='activate("great")'>Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'RatingControl',
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  methods: {
+    activate(option){
+      this.$emit('update:modelValue', option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: aquamarine;
+}
+
+.active button {
+  color: aquamarine;
+}
+
+ul {
+  list-style: none;
+  margin: 0 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  border: none;
+  font: inherit;
+  background-color: transparent;
+}
+</style>
--- Lectures/forms-01-starting-setup/src/components/TheForm.vue
+++ Lectures/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,16 +1,16 @@
 <template>
-  <form>
+  <form @submit.prevent='submitData'>
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model='username'/>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model='age'/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model='referrer'>
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,38 +19,74 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value='news' v-model='interest'/>
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value='tutorials' v-model='interest'/>
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value='nothing' v-model='interest'/>
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value='video' v-model='how'/>
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value='blogs' v-model='how'/>
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value='other' v-model='how'/>
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class='form-control'>
+      <rating-control v-model='rating'></rating-control>
     </div>
     <div>
       <button>Save Data</button>
     </div>
   </form>
 </template>
+
+<script>
+import RatingControl from '@/components/RatingControl';
+export default {
+  components: { RatingControl },
+  data(){
+    return{
+      username: '',
+      age: null,
+      referrer: 'google',
+      interest: [],
+      how: '',
+      rating: null,
+
+    }
+  },
+  methods: {
+    submitData() {
+      console.log(this.username);
+      console.log(this.age);
+      console.log(this.referrer);
+      console.log(this.interest);
+      console.log(this.how);
+      this.username='';
+      this.age=null;
+      this.referrer='google';
+      this.interest=[];
+      this.how='';
+      this.rating=null;
+    }
+  }
+}
+</script>
 
 <style scoped>
 form {
