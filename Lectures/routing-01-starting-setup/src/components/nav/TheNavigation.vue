ce1f83d8b7f6cdf1831940fc9b89b7309002010e
Hari Krishna <harikrishna98@gmail.com>
2021-04-27 16:34:28 +0300

    changed the navigation of the app from using dynamic components, to instead using the vue-router package to navigate between the two pages, each with their own URL

--- Lectures/routing-01-starting-setup/src/components/nav/TheNavigation.vue
+++ Lectures/routing-01-starting-setup/src/components/nav/TheNavigation.vue
@@ -3,10 +3,10 @@
     <nav>
       <ul>
         <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
+          <router-link to='/teams'>Teams</router-link>
         </li>
         <li>
-          <button @click="setActivePage('users-list')">Users</button>
+          <router-link to='/users'>Users</router-link>
         </li>
       </ul>
     </nav>
@@ -15,11 +15,7 @@
 
 <script>
 export default {
-  emits: ['set-page'],
   methods: {
-    setActivePage(page) {
-      this.$emit('set-page', page);
-    },
   },
 };
 </script>
@@ -49,8 +45,8 @@ li {
   margin: 0 2rem;
 }
 
-button {
-  font: inherit;
+a {
+  text-decoration: none;
   background: transparent;
   border: 1px solid transparent;
   cursor: pointer;
@@ -59,8 +55,9 @@ button {
   display: inline-block;
 }
 
-button:hover,
-button:active {
+a:hover,
+a:active,
+a.router-link-active {
   color: #f1a80a;
   border-color: #f1a80a;
   background-color: #1a037e;
--- Lectures/routing-01-starting-setup/src/main.js
+++ Lectures/routing-01-starting-setup/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
-
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import TeamsList from '@/components/teams/TeamsList';
+import UsersList from '@/components/users/UsersList';
 
-const app = createApp(App)
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {path: '/teams', component: TeamsList},
+    {path: '/users', component: UsersList},
+  ]
+});
+
+const app = createApp(App);
+app.use(router);
 app.mount('#app');
