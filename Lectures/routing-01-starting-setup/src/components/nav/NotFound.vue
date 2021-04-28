dca9af9331f81e297d0b1c291cf431afef80f04d
Hari Krishna <harikrishna98@gmail.com>
2021-04-28 15:07:16 +0300

    added redirection from '/' path to /teams, as well as a 'catch-all' route that displays a 404-esque page when user tries to access invalid routes.

---
+++ Lectures/routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+<h2> Page Not Found </h2>
+  <p> Go to <router-link to='/teams'> Teams </router-link> </p>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style scoped>
+
+</style>
--- Lectures/routing-01-starting-setup/src/main.js
+++ Lectures/routing-01-starting-setup/src/main.js
@@ -4,14 +4,17 @@ import App from './App.vue';
 import TeamsList from '@/components/teams/TeamsList';
 import UsersList from '@/components/users/UsersList';
 import TeamMembers from '@/components/teams/TeamMembers';
+import NotFound from '@/components/nav/NotFound';
 
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    {path: '/', redirect: '/teams'},
     {path: '/teams', component: TeamsList},
     {path: '/users', component: UsersList},
     {path: '/teams/:teamId', component: TeamMembers, props:true},
+    {path: '/:notFound(.*)', component: NotFound},
   ]
 });
 
