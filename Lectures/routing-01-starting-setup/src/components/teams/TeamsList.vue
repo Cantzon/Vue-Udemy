a6ef43a74dd9288b91df6b6f8c4fd8df2e6dd1f8
Hari Krishna <harikrishna98@gmail.com>
2021-04-28 16:22:16 +0300

    added individual team lists as nested routes within /teams/

--- Lectures/routing-01-starting-setup/src/components/teams/TeamsList.vue
+++ Lectures/routing-01-starting-setup/src/components/teams/TeamsList.vue
@@ -1,11 +1,12 @@
 <template>
+  <router-view></router-view>
   <ul>
     <teams-item
-      v-for="team in teams"
-      :key="team.id"
+      v-for='team in teams'
+      :key='team.id'
       :id='team.id'
-      :name="team.name"
-      :member-count="team.members.length"
+      :name='team.name'
+      :member-count='team.members.length'
     ></teams-item>
   </ul>
 </template>
@@ -15,9 +16,9 @@ import TeamsItem from './TeamsItem.vue';
 
 export default {
   components: {
-    TeamsItem,
+    TeamsItem
   },
-  inject: ['teams'],
+  inject: ['teams']
 };
 </script>
 
--- Lectures/routing-01-starting-setup/src/main.js
+++ Lectures/routing-01-starting-setup/src/main.js
@@ -10,11 +10,14 @@ import NotFound from '@/components/nav/NotFound';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', redirect: '/teams'},
-    {path: '/teams', component: TeamsList},
-    {path: '/users', component: UsersList},
-    {path: '/teams/:teamId', component: TeamMembers, props:true},
-    {path: '/:notFound(.*)', component: NotFound},
+    { path: '/', redirect: '/teams' },
+    {
+      path: '/teams', component: TeamsList, children: [
+        { path: '/teams/:teamId', component: TeamMembers, props: true }
+      ]
+    },
+    { path: '/users', component: UsersList },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
