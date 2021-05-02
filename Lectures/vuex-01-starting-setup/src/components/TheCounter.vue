faffec64d753df239f21bd3232794f0e6d43c17f
Hari Krishna <harikrishna98@gmail.com>
2021-05-02 17:22:47 +0300

    added incrementing the counter through mutations and 'commit()', instead of directly changing the state from within the components

---
+++ Lectures/vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,18 @@
+<template>
+<h3> {{ counter }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'TheCounter',
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- Lectures/vuex-01-starting-setup/src/main.js
+++ Lectures/vuex-01-starting-setup/src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue';
-
+import {createStore} from 'vuex';
 import App from './App.vue';
 
-const app = createApp(App);
 
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    }
+  },
+  mutations: {
+    increment(state){
+      state.counter++;
+    }
+  }
+})
+const app = createApp(App);
+app.use(store);
 app.mount('#app');
