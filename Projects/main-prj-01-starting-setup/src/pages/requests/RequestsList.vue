197440bcc939763e9f18b101ea5fce40852a88af
Hari Krishna <harikrishna98@gmail.com>
2021-05-17 13:00:55 +0300

    populate requests page with all requests, working next on filtering requests for active coach

--- Projects/main-prj-01-starting-setup/src/pages/requests/RequestsList.vue
+++ Projects/main-prj-01-starting-setup/src/pages/requests/RequestsList.vue
@@ -1,13 +1,52 @@
 <template>
- bruh
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in requests"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet.</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import RequestItem from '@/components/requests/RequestItem';
+
 export default {
-  name: 'RequestsList'
+  name: 'RequestsList',
+  components: { RequestItem },
+  computed: {
+    requests() {
+      return this.$store.getters['requests/getRequests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    }
+  }
 };
 </script>
 
 <style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
--- Projects/main-prj-01-starting-setup/src/store/requests/getters.js
+++ Projects/main-prj-01-starting-setup/src/store/requests/getters.js
@@ -1,5 +1,8 @@
 export default {
   getRequests(state){
     return state.requests;
+  },
+  hasRequests(state) {
+    return state.requests && state.requests.length > 0;
   }
 };
