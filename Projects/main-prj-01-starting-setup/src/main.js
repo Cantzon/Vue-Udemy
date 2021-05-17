81a4ed166dce284e18237d37da97d14ed0829519
Hari Krishna <harikrishna98@gmail.com>
2021-05-17 15:07:55 +0300

    implemented loading spinner that shows when u refresh instead of stale data

--- Projects/main-prj-01-starting-setup/src/main.js
+++ Projects/main-prj-01-starting-setup/src/main.js
@@ -5,6 +5,7 @@ import App from './App';
 import BaseCard from './components/UI/BaseCard';
 import BaseBadge from './components/UI/BaseBadge';
 import BaseButton from './components/UI/BaseButton';
+import BaseSpinner from './components/UI/BaseSpinner';
 
 const app = createApp(App);
 app.use(router);
@@ -12,6 +13,7 @@ app.use(store);
 app.component('base-card', BaseCard);
 app.component('base-badge', BaseBadge);
 app.component('base-button', BaseButton);
+app.component('base-spinner', BaseSpinner);
 app.mount('#app');
 
 
